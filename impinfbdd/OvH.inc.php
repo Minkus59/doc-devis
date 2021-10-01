afbbe58b6c210c6576f11c121d73711562cbb319
Michael Helinckx <82393869+Minkus59@users.noreply.github.com>
2021-10-01 09:24:16 +0200

    Update OvH.inc.php

--- impinfbdd/OvH.inc.php
+++ impinfbdd/OvH.inc.php
@@ -5,7 +5,7 @@
 
 $Ovh_serv_bDd = "mysql:host=esoftfrxiominkus.mysql.db;dbname=esoftfrxiominkus";
 $uTil_bDd_serv="esoftfrxiominkus";
-$mDp_bDd_serv="Cqdfx301";
+$mDp_bDd_serv="XXXXXX";
 
 $Home="http://devis.e-soft.fr";
 $Home2="http://devis.e-soft.fr";
@@ -15,11 +15,11 @@ $Societe="NeuroSoft Team";
 $Publisher="Helinckx Michael";
 $Prefix="";
 
-//$MAJ="Une mise à  jour a eu lieu ! <BR />Pour tous dysfonctionnement, veuillez contacter l'administrateur du logiciel à  : contact@neuro-soft.fr<BR />";
-//$MAJ="Quelques difficultés ont été identifié concernant l'envoi des devis et facture par email.<BR />
-//Nous mettons tous en oeuvre afin de rétablir ce service dans les plus brefs délais";
+//$MAJ="Une mise Ã Â  jour a eu lieu ! <BR />Pour tous dysfonctionnement, veuillez contacter l'administrateur du logiciel Ã Â  : contact@neuro-soft.fr<BR />";
+//$MAJ="Quelques difficultÃ©s ont Ã©tÃ© identifiÃ© concernant l'envoi des devis et facture par email.<BR />
+//Nous mettons tous en oeuvre afin de rÃ©tablir ce service dans les plus brefs dÃ©lais";
 
-//$Adresse="324 rue Jean Jaurès, 59170 Croix";
+//$Adresse="324 rue Jean JaurÃ¨s, 59170 Croix";
 //$Telephone="03 20 64 54 22";
 
 $GoogleMap='<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3169.9370902026567!2d3.1568475159413696!3d50.681904478540915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c328f934d0a3ed%3A0x343a66bf2369fd2a!2s324+Rue+Jean+Jaur%C3%A8s%2C+59170+Croix!5e1!3m2!1sfr!2sfr!4v1459864299038" width="100%" height="350" frameborder="0" style="border:0"></iframe>';
